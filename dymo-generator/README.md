c5bbaa358402870db7b58e7446b33fb693d5288a
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2016-05-09 08:22:54 +0000

    try dymo-generator with rdf - errors

--- dymo-generator/README.md
+++ dymo-generator/README.md
@@ -14,3 +14,10 @@ Non-event features are added as features. In the case of RDF this may only be MF
 
 
 Chris Greenhalgh, The University of Nottingham, 2016
+
+## Issues
+
+initial `test.js`, which tries to do DymoTemplates.createAnnotatedBarAndBeatDymo
+- if reading bar and beats from JSON, output single top-level dymo with no time information
+- if reading tempo from JAMS, throws exception with .value of udefined in DymoGenerator.addFeature, possibly because time is primitive.
+- if reading bar and beats from n3, seems to be a similar problem with split of undefined perhaps on subject or object of af:value (which is not present in n3).
--- dymo-generator/test.js
+++ dymo-generator/test.js
@@ -4,11 +4,25 @@ var fs = require("fs");
 var math = require('./bower_components/mathjs/dist/math.js');
 eval(fs.readFileSync('bower_components/dymo-core/dist/dymo-core.min.js')+'');
 eval(fs.readFileSync('bower_components/dymo-generator/dist/dymo-generator.min.js')+'');
+// RDF
+var N3 = require('n3');
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 var loader = new FeatureLoader();
-var barbeat =JSON.parse(fs.readFileSync('Stella_vamp_qm-vamp-plugins_qm-barbeattracker_beats.json'));
-var tempo = JSON.parse(fs.readFileSync('Stella_vamp_qm-vamp-plugins_qm-tempotracker_tempo.json'));
+// JSON
+//var barbeat =JSON.parse(fs.readFileSync('Stella_vamp_qm-vamp-plugins_qm-barbeattracker_beats.json'));
+//var tempo = JSON.parse(fs.readFileSync('Stella_vamp_qm-vamp-plugins_qm-tempotracker_tempo.json'));
 var source = 'Stella.wav'
+
+//var json = tempo; //barbeat;
+//console.log('hoping for file_metadata: '+JSON.stringify(Object.keys(json)[0]));
+//console.log('hoping for annotations: '+JSON.stringify(Object.keys(json)[1]));
+//console.log('output_id: '+json[Object.keys(json)[1]][0].annotation_metadata.annotator.output_id);
+
+// RDF
+var barbeat = "file:///Stella_vamp_qm-vamp-plugins_qm-barbeattracker_beats.n3";
+var tempo = "file:///Stella_vamp_qm-vamp-plugins_qm-tempotracker_tempo.n3";
+
 
 var generator = new DymoGenerator(undefined, function(){});
 
@@ -17,7 +31,7 @@ generator.setCondensationMode(MEAN);
 //loader.loadFeature(barbeat, undefined, generator, function() { console.log('loaded barbeat'); });
 
 //if (false) 
-DymoTemplates.createAnnotatedBarAndBeatDymo(generator, [barbeat /*, tempo*/], function() {
+DymoTemplates.createAnnotatedBarAndBeatDymo(generator, [barbeat/*, tempo*/], function() {
 	var dymo = generator.getDymo();
 	dymo.setSourcePath(source);
 	fs.writeFile('dymo.json', JSON.stringify(dymo.toJsonHierarchy()));
